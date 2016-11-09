5ec070e186b3d5b55e6ea792b292706a2e5a9769
ZhengXiong Zhao <zxzhao@whoami.local>
2016-11-10 00:08:25 +0800

    first commit

---
+++ packaging/osx/build_dmg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+  echo 'build_dmg.sh version'
+  exit 1
+fi
+
+dmgbuild -s settings.py 'Shadowsocks' ShadowsocksX-$1.dmg
+# rsync --progress -e ssh ShadowsocksX-$1.dmg frs.sourceforge.net:/home/frs/project/shadowsocksgui/dist/
+# rsync --progress -e ssh ShadowsocksX-$1.dmg frs.sourceforge.net:/home/frs/project/ShadowsocksX/dist/
+
